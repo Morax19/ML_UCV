a9c1ba8a317cf3e972264543edc9ea98d6c6e1f6
Morax19 <danielhdez19@gmail.com>
2025-02-17 11:55:52 -0400

    .

--- Tarea2/ml-tarea2B-knn-svm/src/pages/01_A_Jugar.py
+++ Tarea2/ml-tarea2B-knn-svm/src/pages/01_A_Jugar.py
@@ -53,7 +53,7 @@ def play_canvas():
     operator_1 = st.empty()
 
     # Definir el lienzo donde los usuarios dibujarán
-    number_1.image_data = st.canva(
+    number_1.image_data = st.canvas(
         width=280,
         height=280,
         stroke_width=stroke_width,
